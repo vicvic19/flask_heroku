2fb8fecb42f66419cc70af93dac82246dfc58a78
victor <victor.saldias@ing.uchile.cl>
2021-05-26 19:39:13 -0400

    changes in hello outside

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 from flask import Flask
 print("Logro llegar al archivo de afuera")
+
 app = Flask(__name__)
 
+print("Logro llegar al archivo de afuera parte 2")
 @app.route('/')
+
+print("Logro llegar al archivo de afuera parte 3")
 def hello_world():
     return('Hello World Victor de más afuera!')
